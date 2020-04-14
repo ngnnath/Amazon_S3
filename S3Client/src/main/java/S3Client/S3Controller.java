1aabcb7221630e1e9db05a80c1ab72115b1022fc
ngnnath <nguyen.nathalie75@hotmail.fr>
2020-04-14 09:57:47 -0400

    delete and get

--- S3Client/src/main/java/S3Client/S3Controller.java
+++ S3Client/src/main/java/S3Client/S3Controller.java
@@ -3,6 +3,7 @@ package S3Client;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -10,15 +11,14 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
 
@@ -27,7 +27,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 /**
  * Controller S3
  */
-@Controller
+@RestController
 public class S3Controller {
 
     /**
@@ -81,7 +81,7 @@ public class S3Controller {
                 .withClientConfiguration(clientConfiguration)
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
-
+        String result= "";
         try {
             System.out.println("Uploading a new object to S3 from a file\n");
             File file = new File(uploadFileName);
@@ -93,6 +93,7 @@ public class S3Controller {
             S3Object objectPortion = s3Client.getObject(rangeObjectRequest);
             System.out.println("Printing bytes retrieved:");
             displayTextInputStream(objectPortion.getObjectContent());
+            result ="Fichier àjouté";
         } catch (AmazonServiceException ase) {
             System.out.println("Caught an AmazonServiceException, which " + "means your request made it "
                     + "to Amazon S3, but was rejected with an error response" + " for some reason.");
@@ -101,14 +102,16 @@ public class S3Controller {
             System.out.println("AWS Error Code:   " + ase.getErrorCode());
             System.out.println("Error Type:       " + ase.getErrorType());
             System.out.println("Request ID:       " + ase.getRequestId());
+            result="erreur";
 
         } catch (AmazonClientException ace) {
             System.out.println("Caught an AmazonClientException, which " + "means the client encountered " + "an internal error while trying to "
                     + "communicate with S3, " + "such as not being able to access the network.");
             System.out.println("Error Message: " + ace.getMessage());
+            result="erreur";
 
         }
-        return "done";
+        return result;
     }
 
     private static void displayTextInputStream(InputStream input) throws IOException {
@@ -123,5 +126,92 @@ public class S3Controller {
         }
         System.out.println();
     }
+
+    /**
+     * Get the file
+     * @param key clé de l'objet à récupérer
+     * @throws IOException error in DisplayTextStream
+     */
+    @RequestMapping(value = "/get/file", method = GET)
+    public void getFile(@RequestParam("key") String key) throws IOException {
+
+        S3Object fullObject = null, objectPortion = null, headerOverrideObject = null;
+        AWSCredentials credentials = new BasicAWSCredentials(app.getAccessKey(), app.getAccessSecret());
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(app.getUrl(), Regions.US_EAST_1.name()))
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .build();
+
+            // Get an object and print its contents.
+            System.out.println("Downloading an object");
+            fullObject = s3Client.getObject(new GetObjectRequest(app.getBucketname(), key));
+            System.out.println("Content-Type: " + fullObject.getObjectMetadata().getContentType());
+            System.out.println("Content: ");
+            displayTextInputStream(fullObject.getObjectContent());
+
+            // Get a range of bytes from an object and print the bytes.
+            GetObjectRequest rangeObjectRequest = new GetObjectRequest(app.getBucketname(), key)
+                    .withRange(0, 9);
+            objectPortion = s3Client.getObject(rangeObjectRequest);
+            System.out.println("Printing bytes retrieved.");
+            displayTextInputStream(objectPortion.getObjectContent());
+
+            // Get an entire object, overriding the specified response headers, and print the object's content.
+            ResponseHeaderOverrides headerOverrides = new ResponseHeaderOverrides()
+                    .withCacheControl("No-cache")
+                    .withContentDisposition("attachment; filename=example.txt");
+            GetObjectRequest getObjectRequestHeaderOverride = new GetObjectRequest(app.getBucketname(), key)
+                    .withResponseHeaders(headerOverrides);
+            headerOverrideObject = s3Client.getObject(getObjectRequestHeaderOverride);
+            displayTextInputStream(headerOverrideObject.getObjectContent());
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        } finally {
+            // To ensure that the network connection doesn't remain open, close any open input streams.
+            if (fullObject != null) {
+                fullObject.close();
+            }
+            if (objectPortion != null) {
+                objectPortion.close();
+            }
+            if (headerOverrideObject != null) {
+                headerOverrideObject.close();
+            }
+        }
+    }
+
+    @RequestMapping(value = "/delete/file", method = GET)
+    @ResponseBody
+    public String deleteFile(@RequestParam("key") String key){
+        String result="";
+        AWSCredentials credentials = new BasicAWSCredentials(app.getAccessKey(), app.getAccessSecret());
+        try {
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(app.getUrl(), Regions.US_EAST_1.name()))
+                    .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                    .build();
+
+            s3Client.deleteObject(new DeleteObjectRequest(app.getBucketname(), key));
+            result="Delete Done";
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
 
 }
